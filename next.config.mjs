a08a0a8206b1bcad2b03978a107c79a47c7b6fd2
Samuel K. Kihoon <SamuelTheGalaxys@gmail.com>
2025-04-27 13:21:01 +0900

    Fix basePath for Github Pages

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+
+  // 여기가 추가된 부분
+  basePath: '/birzontHomes',
+  assetPrefix: '/birzontHomes/',
+  trailingSlash: true,
 }
 
 export default nextConfig
